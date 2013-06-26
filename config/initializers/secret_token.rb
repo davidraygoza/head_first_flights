4e9729a2d7356a63b098a06ac77f08b78f3cf362
david raygoza <david_raygoza@hotmail.com>
2013-06-25 22:10:49 -0500

    Add seats from flights

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coconut::Application.config.secret_token = 'b6cf9541de1674952161019d410e7df6e490b3ab1edeb9fdc1064e228df5b7e67c4f1ea166f0f6bcefaabef10406580c5405c312b7c896a14e9d980b928d6c00'
