ecb09339de86a78ff6b71552b48257360e34343c
david raygoza <david_raygoza@hotmail.com>
2013-06-25 23:21:35 -0500

    Validates fllight capacity

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,3 +1,18 @@
 class Seat < ActiveRecord::Base
   attr_accessible :baggage, :flight_id, :name
+ 
+     validates :baggage, :numericality => { :less_than_or_equal_to => :baggage_allowance }, :presence => true 
+     validate :seats_flight
+ 
+  belongs_to :flight
+ 
+
+  def seats_flight
+    errors.add(:flight_id,"The flight is full") if (flight.seats.size >= flight.capacity )
+  end 
+
+  def baggage_allowance
+    flight.baggage_allowance
+  end  
+  
 end
